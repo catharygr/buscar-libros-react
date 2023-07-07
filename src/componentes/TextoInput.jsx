4f20c32af91f3165349e24a39e70f43d0ba57303
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-07 18:25:23 +0100

    Trabajando en el componente TextoInput y su css

--- src/componentes/TextoInput.jsx
+++ src/componentes/TextoInput.jsx
@@ -1,7 +1,14 @@
+// import { useId } from "react";
+// Función que genera un id único
 export default function TextoInput() {
+  // let generarId = useId();
+  // let apliacadoId = id || generarId;
   return (
-    <>
-      <label htmlFor="buscar">Buscar</label>
-    </>
+    <div className="container-input">
+      <label className="label-buscar" htmlFor="buscar">
+        Buscar
+      </label>
+      <input type="text" id="buscar" />
+    </div>
   );
 }
