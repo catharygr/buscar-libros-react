515255f70e988c91416c6e7bc8ba9bf3bb6b768e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-08 17:56:15 +0100

    Trabajando en la lógica de la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,29 @@ import TextoInput from "./componentes/TextoInput";
 import { useState } from "react";
 
 const ENDPOINT =
-  "https://www.omdbapi.com/?i=tt3896198&apikey=b4ee78c6&${type}=${searchInput}";
+  "https://www.omdbapi.com/?i=tt3896198&apikey=b4ee78c6&${type}=${buscarInput}";
 
 export default function App() {
-  const [searchInput, setSearchInput] = useState("");
+  const [buscarInput, setBuscarinput] = useState("");
   const [buscarResultados, setBuscarResultados] = useState([]);
 
   // inactivo, cargando, exito, sin exito, error
   const [estado, setEstado] = useState("inactivo");
 
+  // Funcion para manejar el submit del formulario
   async function handleSubmit(e) {
     e.preventDefault();
     setEstado("cargando");
-    const url = `${ENDPOINT} ? searchInput=${searchInput}`;
+    const url = `${ENDPOINT} ? buscarInput=${buscarInput}`;
     const res = await fetch(url);
     const data = await res.json();
+    if (data.ok) {
+      setBuscarResultados(data.Search);
+      buscarInput("");
+      data?.Search?.length > 0 ? setEstado("exito") : setEstado("sin exito");
+    } else {
+      setEstado("error");
+    }
     console.log(data);
   }
 
@@ -32,8 +40,8 @@ export default function App() {
           <TextoInput
             label="Buscar"
             placeholder="Busca una película"
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
+            value={buscarInput}
+            onChange={(e) => setBuscarinput(e.target.value)}
           />
           <button className="btn" type="submit">
             Ir
@@ -41,7 +49,15 @@ export default function App() {
         </form>
       </header>
       <main>
-        <section className="section-buscar">{mapeo}</section>
+        <section className="section-buscar">
+          <h2>Buscar resultados:</h2>
+          {estado === "inactivo" && <p>Busca una película</p>}
+          {estado === "cargando" && <p>Cargando...</p>}
+          {estado === "exito" && <p>Resultados encontrados</p>}
+          {estado === "sin exito" && <p>Sin resultados</p>}
+          {estado === "error" && <p>Hubo un error</p>}
+          {mapeo}
+        </section>
       </main>
     </>
   );
--- src/componentes/BuscarResultado.jsx
+++ src/componentes/BuscarResultado.jsx
@@ -17,21 +17,3 @@ export default function BuscarResultado(result) {
     </div>
   );
 }
-
-/*  <div class="container">
-<header>
-<div class="title-nav">
-  <h1>Find your film</h1>
-  <p>
-    <a href="./mi-lista.html">My watchlist</a>
-  </p>
-</div>
-<div class="search-form">
-  <input id="search-field" class="search" type="text" placeholder="Movie name here...">
-  <button id="search-btn" class="btn">Search</button>
-</div>
-</header>
-<main id="main-container" class="main-container">
-<p>Search results shows here...</p>
-</main>
-</div>   */
