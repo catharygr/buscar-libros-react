798bfa4d0aaef168b03c8b1e9b1c7d98165a963e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-12 17:00:14 +0100

    Trabajando en css

--- src/componentes/BuscarResultado.jsx
+++ src/componentes/BuscarResultado.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/prop-types */
 export default function BuscarResultado({ result }) {
   return (
-    <article className="buscar-container">
-      <div className="buscar-resultado__img">
-        <img src={result.Poster} alt={result.Title} />
-      </div>
-      <div className="buscar-resultado__info">
-        <h1>{result.Title}</h1>
-        <p>{result.Year}</p>
-        <p>{result.Type}</p>
-      </div>
-    </article>
+    <div className="buscar-container">
+      <article className="buscar-resultado">
+        <div className="buscar-resultado__img">
+          <img src={result.Poster} alt={result.Title} />
+        </div>
+        <div className="buscar-resultado__info">
+          <h1>{result.Title}</h1>
+          <p>{result.Year}</p>
+          <p>{result.Type}</p>
+        </div>
+      </article>
+    </div>
   );
 }
